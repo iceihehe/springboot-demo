09584a49a7b21084202013c3b6acd0d49f597938
iceihehe <iceihehe@gmail.com>
2020-07-31 17:45:00 +0800

    分环境读取配置文件

--- cm-web/src/test/java/com/iceihehe/cm/web/PaginationTest.java
+++ cm-web/src/test/java/com/iceihehe/cm/web/PaginationTest.java
@@ -5,6 +5,7 @@ import com.iceihehe.cm.dao.mapper.MTaskMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
 public class PaginationTest {
     @Resource
     MTaskMapper mapper;
